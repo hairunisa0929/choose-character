d22837079afad4a56cc3057f7f6de997b075c13f
hairunisa0929 <hairunisa.learning@gmail.com>
2023-10-15 13:02:53 +0700

    create checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/Layout";
 import DetailPage from "./pages/DetailPage";
 import LandingPage from "./pages/LandingPage";
 import CharacterPage from "./pages/CharacterPage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/characters" element={<CharacterPage />} />
         <Route path="/characters/:id" element={<DetailPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </Layout>
   );
