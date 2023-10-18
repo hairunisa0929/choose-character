a7d92e352721f318b1968597073852a1d5313323
hairunisa0929 <hairunisa.learning@gmail.com>
2023-10-18 23:31:20 +0700

    fix action checkout

--- src/store/actions/checkoutAction.js
+++ src/store/actions/checkoutAction.js
@@ -1,8 +1,6 @@
 import { CHECKOUT_BOOKING } from "../types";
 
-export const checkoutBooking = (payload) => (dispatch) => {
-  dispatch({
-    type: CHECKOUT_BOOKING,
-    payload: payload,
-  });
-};
+export const checkoutBooking = (payload) => ({
+  type: CHECKOUT_BOOKING,
+  payload,
+});
